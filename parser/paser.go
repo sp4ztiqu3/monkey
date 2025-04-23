96b37540408a71c29a55245eb3b5226a75be16e0
Matt Hiddle <matt.hiddle@gmail.com>
2025-04-23 22:38:06 +0800

    parser: add ability for parser to recognize errors

--- parser/paser.go
+++ parser/paser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/sp4ztiqu3/monkey/ast"
 	"github.com/sp4ztiqu3/monkey/lexer"
 	"github.com/sp4ztiqu3/monkey/token"
@@ -11,16 +13,26 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
 	// Read two tokens, so that curToken and peekToken are both select {
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -86,6 +98,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
